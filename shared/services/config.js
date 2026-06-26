7a3bd49a679474cec48de952eddf831db51321ad
Juanexee <juankpaladino@gmail.com>
2026-06-26 00:32:45 -0600

    pirilin pinpon

--- shared/services/config.js
+++ shared/services/config.js
@@ -1,6 +1,6 @@
 // shared/services/config.js
 
-export const BASE_URL = "https://backend.rancholamimi.com:7081";
+export const BASE_URL = "https://backend.rancholamimi.com";
 
 // Para compatibilidad
 export default { BASE_URL };
